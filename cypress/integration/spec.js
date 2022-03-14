76f774f45519355a2b6b90208c4027e3a5aebffd
Kirill Fedoseev <k.fedoseev@innopolis.university>
2022-03-15 02:42:30 +0400

    Add cypress

---
+++ cypress/integration/spec.js
@@ -0,0 +1,26 @@
+describe('horse racing test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('displays title', () => {
+    cy.get('.title').should('contain', 'Blockchain Horse Racing')
+  })
+
+  it('jumps to game on Play button', () => {
+    cy.get('.playButton').click()
+
+    cy.location('href').should('contain', '/horse-racing')
+    cy.contains('Horse A')
+    cy.contains('Horse B')
+    cy.contains('Horse C')
+  })
+
+  it('should place a bet icon', () => {
+    cy.get('.playButton').click()
+
+    cy.get('.horseState').should('have.length', 3).first().click()
+
+    cy.get('.horseState').first().should('contain.html', 'betIcon')
+  })
+})
--- src/pages/horseRacing/consts.ts
+++ src/pages/horseRacing/consts.ts
@@ -1,18 +1,18 @@
 export const initialHorses = [
   {
-    name: 'Zhopa',
+    name: 'Horse A',
     color: '#fdfffcff',
     position: '80%',
     isBetOn: false,
   },
   {
-    name: 'Piska',
+    name: 'Horse B',
     color: '#41ead4ff',
     position: '60%',
     isBetOn: false,
   },
   {
-    name: 'Kal',
+    name: 'Horse C',
     color: '#ff0022ff',
     position: '30%',
     isBetOn: false,
